5cec068240312aa49483f8afd85486bd1a132c06
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-06-13 20:18:49 +0900

    add hardhat 	:fire:

---
+++ hardhat.config.ts
@@ -0,0 +1,47 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname+'/.env' });
+import "@nomiclabs/hardhat-etherscan";
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const local = {
+  solidity: "0.7.0",
+  networks: {
+    hardhat: {
+    },
+  },
+};
+
+const with_kovan = {
+  solidity: "0.7.0",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ALCHEMY_MAINNET_RPC_URL
+      }
+    },
+    kovan: {
+      url: process.env.ALCHEMY_KOVAN_RPC_URL,
+      accounts: [`0x${process.env.KOVAN_PRIVATE_KEY}`]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
+
+const config = process.env.ALCHEMY_KOVAN_RPC_URL ? with_kovan : local;
+
+export default config;
