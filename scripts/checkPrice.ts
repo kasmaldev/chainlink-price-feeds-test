d348b85f8efdbafa29a5bd97dcc84f518f4c4223
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-06-15 21:25:04 +0900

    interact with deployed functions

--- scripts/checkPrice.ts
+++ scripts/checkPrice.ts
@@ -1,53 +1,21 @@
-import { run, ethers } from "hardhat";
-// import PriceConverterABI from "../artifacts/contracts/PriceConverter.sol/PriceConverter.json";
+import { ethers } from "hardhat";
+import { formatEther } from "@ethersproject/units";
 
 async function main() {
 
-    const PriceConverterABI = [
-    {
-      "inputs": [],
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "inputs": [
-        {
-          "internalType": "int256",
-          "name": "input",
-          "type": "int256"
-        }
-      ],
-      "name": "convertPrice",
-      "outputs": [
-        {
-          "internalType": "int256",
-          "name": "",
-          "type": "int256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "getThePrice",
-      "outputs": [
-        {
-          "internalType": "int256",
-          "name": "",
-          "type": "int256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ]
-    const provider = ethers.provider;
-    const contractAddress = "0xe4D23c26945Bb14DaD7Fdf1eccF1279Ac92612D4"
-    const converterContract = new ethers.Contract(contractAddress, PriceConverterABI, provider)
 
-	console.log(await converterContract.getThePrice());
-	console.log(await converterContract.convertPrice(50000));
+    const contractAddress = "0xe4D23c26945Bb14DaD7Fdf1eccF1279Ac92612D4"
+    const PriceConverterContract = await ethers.getContractFactory("PriceConverter");
+    const contract = await PriceConverterContract.attach(contractAddress);
+    const latestPrice = await contract.getThePrice();
+    const convertPrice = await contract.convertPrice(50000);
+
+    // useful link: https://ethereum.stackexchange.com/questions/95023/hardhat-how-to-interact-with-a-deployed-contract
+
+    console.log(latestPrice.toNumber())
+    console.log(
+        formatEther(latestPrice), formatEther(convertPrice)
+    )
 
 }
 
