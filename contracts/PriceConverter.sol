0724ee85cd4a477c0c4a8f5f0dc9b5c6a8c07910
yuichiroaoki <yuichiroaoki27@gmail.com>
2021-06-16 12:09:27 +0900

    add jpy/usd contract

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -4,15 +4,20 @@ pragma solidity 0.7.0;
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
 contract PriceConverter {
-    AggregatorV3Interface internal priceFeed;
+    AggregatorV3Interface internal eth_usd_price_feed;
+    AggregatorV3Interface internal jpy_usd_price_feed;
 
     /**
      * Network: Kovan
      * Aggregator: ETH/USD
      * Address: 0x9326BFA02ADD2366b30bacB125260Af641031331
+
+     * Aggregator: JPY/USD
+     * Address: 0xBcE206caE7f0ec07b545EddE332A47C2F75bbeb3
      */
     constructor() {
-        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+        eth_usd_price_feed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+        jpy_usd_price_feed = AggregatorV3Interface(0xBcE206caE7f0ec07b545EddE332A47C2F75bbeb3);
     }
 
     /**
@@ -25,7 +30,7 @@ contract PriceConverter {
             uint startedAt,
             uint timeStamp,
             uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
+        ) = eth_usd_price_feed.latestRoundData();
 
         return price;
     }
@@ -37,7 +42,15 @@ contract PriceConverter {
             uint startedAt,
             uint timeStamp,
             uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
+        ) = eth_usd_price_feed.latestRoundData();
+
+        (
+            uint80 jroundID, 
+            int jprice,
+            uint jpystartedAt,
+            uint jpytimeStamp,
+            uint80 jpyansweredInRound
+        ) = jpy_usd_price_feed.latestRoundData();
 
         require(input > 0, "The input should be a positive number.");
 
